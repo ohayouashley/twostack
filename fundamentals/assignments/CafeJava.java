ea7ee28d8715cbeaaceb5a111d6324634cb6cdf0
Ashley Chase <chaseashley91@gmail.com>
2022-09-01 20:17:45 -0700

    Cafe Java Assignment

---
+++ fundamentals/assignments/CafeJava.java
@@ -0,0 +1,35 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double americanoPrice = 2.5;
+        double cafeLattePrice = 3.5;
+        double londonFogPrice = 2.5;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "MandaSee";
+        String customer3 = "MamRose";
+        String customer4 = "LeenQue";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isNotReadyOrder1 = true;
+        boolean isReadyOrder2 = true;
+
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        // System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // System.out.println(pendingMessage + customer2);
+        System.out.println(displayTotalMessage + mochaPrice + isReadyOrder2 );
+    	// ** Your customer interaction print statements will go here ** //
+    }
+}
---
+++ fundamentals/workspace/Conditionals.java
@@ -0,0 +1,20 @@
+public class Conditionals {
+    public static void main (String[] args) {
+    
+    boolean isRaining = true;
+    boolean isCold = true;
+
+    if(isRaining) {
+        System.out.println("Bring an umbrella");
+    }
+    else if {
+        System.out.println("Have fun!");
+    }
+    else {
+        System.out.println("Wear a coat");
+    }
+        
+    }
+}
+
+print(isRaining);
